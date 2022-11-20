2b05d0494b59cfec2e18355d70b58609a0076a35
Lukas Chyle <lukas_chyle@hotmail.com>
2022-11-20 09:40:45 +0100

    GameLogic/protocol class

---
+++ src/main/java/server/Game.java
@@ -0,0 +1,17 @@
+package server;
+
+public class Game {
+
+    private ServerConnection p1, p2;
+
+    protected void setPlayers(ServerConnection p1, ServerConnection p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    protected void protocol(Data data) {
+        p1.sendData(data); // test
+        p2.sendData(data); // test
+    }
+
+}
