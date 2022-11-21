17ba4959e30c626729f11dd017b0822bcd703da1
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-11-21 16:00:18 +0100

    Changed Data Class var's to public

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 public class Data implements Serializable {
 
+    public String typeOfData;
+
     public int player;
     public int avatar;
-    public String nickname;
+    public String opponentNickname;
     // data.Data object to send between Server side (ConnectionHandler) and Client.
     // Remember that this class have to be identical for client and server.
 
