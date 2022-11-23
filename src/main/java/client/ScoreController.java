f79f4af2b8cfb46c646dcd65ef13fdf38bc09b59
Amar <amards92@gmail.com>
2022-11-23 14:20:17 +0100

    Implemented setScore method for scoreboard

---
+++ src/main/java/client/ScoreController.java
@@ -0,0 +1,20 @@
+package client;
+
+import javafx.scene.Node;
+
+public class ScoreController {
+
+
+
+
+    public void setScore(Boolean[] roundScore, ClientConnection connection) {
+
+
+
+
+    }
+
+
+
+}
+
