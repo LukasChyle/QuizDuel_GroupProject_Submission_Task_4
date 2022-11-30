113a1c1b8b77f48d120c69401fb775a68485accf
Marcus Edlund <marcus.edlund88@gmail.com>
2022-11-30 10:32:32 +0100

    Sorting method inside GetHighScore is now working. Fixed iteration/addToList bug

--- src/main/java/server/GetHighScore.java
+++ src/main/java/server/GetHighScore.java
@@ -130,7 +130,7 @@ public class GetHighScore {
     }
 
 
-  /* public static void main(String[] args) {
+ /* public static void main(String[] args) {
 
         Boolean[] b = {false,false,false};
         Boolean[] b1 = {false,false,false};
